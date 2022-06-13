5cbaba7c35d6facee84bb461a8b7c0ef8e10b989
Kleiton Barone Semolini <kleitonpolar6@gmail.com>
2022-06-13 00:32:48 -0300

    Fixing jest Tests

--- src/app/users/users.service.spec.ts
+++ src/app/users/users.service.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from 'test/mockRepository';
+import { MockType } from 'test/mockType';
+import { Repository } from 'typeorm';
+import { UsersEntity } from './users.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  //let repositoryMock: MockType<Repository<UsersEntity>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UsersEntity),
+          useClass: Repository,
+          //useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    //repositoryMock = module.get(getRepositoryToken(UsersEntity));
   });
 
   it('should be defined', () => {
